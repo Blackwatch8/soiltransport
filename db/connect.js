5b4f2d62a4b7e8edf051afe662c75629cb8f4a49
SoloIsh <ishanhherath12@.com>
2020-05-14 17:34:40 +0530

    database pool added

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,19 @@
 const mysql= require('mysql');
 const config= require('../config');
-Connection = mysql.createConnection(config.mysql);
+const Sequelize=require("sequelize")
+//Connection = mysql.createConnection(config.mysql);
+const Connection= new Sequelize("heroku_56236b0149670d1","bcf5aad9fc6b4b","",{
+    host:"us-cdbr-east-06.cleardb.net",
+    dialect:"mysql",
+    operaorsAliases: false,
 
+    pool:{
+        max:10,
+        min:0,
+        aquire:30000,
+        idle: 10000
+    }
+}) 
 module.exports={
     Connection
 }

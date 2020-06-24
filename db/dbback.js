de84ef9fa1e2fe1a55f83cfd19ecb1e09608cd2b
SoloIsh <ishanhherath12@.com>
2020-06-24 17:30:21 +0530

    backup

--- db/dbback.js
+++ db/dbback.js
@@ -13,7 +13,7 @@ var nodeJsZip = require("nodeJs-zip");
 var dir = path.join(__dirname,"../dbBackups");
 
 // database is dumpded every day 4.30 pm
-const job=cron1.job('45 * * * *', () => {
+const job=cron1.job('5 * * * *', () => {
     console.log("task running")
     fs.readdir(dir, (err, files) => {
         if (err) throw err;
